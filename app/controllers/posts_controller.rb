a9abf644474298014ca625595b2a9065a368f4fb
Anshul Shanker <anshulshankar@gmail.com>
2016-02-23 17:42:52 -0600

    part 1 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,8 +14,10 @@ class PostsController < ApplicationController
 
   def create
     if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
+      flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
       render :new
     end
   end
@@ -25,8 +27,10 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
+      flash[:success] = "Post updated."
       redirect_to posts_path
     else
+      flash.now[:alert] = "Update failed.  Please check the form."
       render :edit
     end
   end
@@ -45,4 +49,4 @@ class PostsController < ApplicationController
   def set_post
     @post = Post.find(params[:id])
   end
-end 
+end
