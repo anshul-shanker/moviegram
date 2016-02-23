af826824f1ce7db438c0a382ae8c7b99b490ba80
Anshul Shanker <anshulshankar@gmail.com>
2016-02-22 23:48:53 -0600

    ability to click one of your posts and it opens

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,27 @@
 class PostsController < ApplicationController
 
 def index
-@posts = Post.all
+  @posts = Post.all
 end
 
 def new
-@post = Post.new
+  @post = Post.new
 end
 
 def create
-@post = Post.create(post_params)
+  @post = Post.create(post_params)
   redirect_to posts_path
 end
+
+def show  
+  @post = Post.find(params[:id])
+end 
 
 private
 
 def post_params  
   params.require(:post).permit(:image, :caption)
 end 
+
 
 end
