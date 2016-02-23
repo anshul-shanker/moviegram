ba30592c2c4390fb81155086fed005316364c8cc
Anshul Shanker <anshulshankar@gmail.com>
2016-02-23 00:06:53 -0600

    edit post done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,8 +17,18 @@ def show
   @post = Post.find(params[:id])
 end 
 
-private
+def edit  
+  @post = Post.find(params[:id])
+end  
 
+def update  
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to(post_path(@post))
+end  
+
+
+private
 def post_params  
   params.require(:post).permit(:image, :caption)
 end 
