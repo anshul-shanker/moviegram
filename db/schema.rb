47faf74374996312dddeb477bbde8e146ceb4a0e
Anshul Shanker <anshulshankar@gmail.com>
2016-02-27 16:51:53 -0600

    user_id in posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224013657) do
+ActiveRecord::Schema.define(version: 20160227224901) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20160224013657) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
