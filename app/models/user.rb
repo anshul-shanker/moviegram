af492755ca6b9eb04360023462a14f8fbac0409a
Anshul Shanker <anshulshankar@gmail.com>
2016-02-28 18:59:13 -0600

    Ajax and js for comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts, dependent: :destroy 
+   has_many :comments, dependent: :destroy
 end
